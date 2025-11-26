d7f0d626603186400c5730fd4b0db7e516180bb9
cavegvng <cavegvng@gmail.com>
2025-11-26 14:54:30 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,14 +16,39 @@ export default defineConfig({
     headers: {
       'Content-Security-Policy': `
         default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.twitter.com https://www.instagram.com https://cdn.syndication.twimg.com;
-        style-src 'self' 'unsafe-inline' https://platform.twitter.com https://ton.twimg.com https://fonts.googleapis.com;
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+          https://platform.twitter.com 
+          https://www.instagram.com 
+          https://cdn.syndication.twimg.com
+          https://www.tiktok.com;
+        style-src 'self' 'unsafe-inline' 
+          https://platform.twitter.com 
+          https://ton.twimg.com 
+          https://fonts.googleapis.com;
         img-src 'self' data: https: http: blob:;
         font-src 'self' data: https://platform.twitter.com https://ton.twimg.com https://fonts.gstatic.com;
-        connect-src 'self' https://*.supabase.co wss://*.supabase.co https://www.youtube.com https://publish.twitter.com https://graph.facebook.com https://syndication.twitter.com https://cdn.syndication.twimg.com;
-        frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://platform.twitter.com https://www.instagram.com https://youtube.com https://youtu.be;
+        connect-src 'self' 
+          https://*.supabase.co 
+          wss://*.supabase.co 
+          https://www.youtube.com 
+          https://publish.twitter.com 
+          https://graph.facebook.com 
+          https://syndication.twitter.com 
+          https://cdn.syndication.twimg.com;
+        frame-src 'self' 
+          https://www.youtube.com 
+          https://www.youtube-nocookie.com 
+          https://platform.twitter.com 
+          https://www.instagram.com 
+          https://youtube.com 
+          https://youtu.be
+          https://www.tiktok.com;
         media-src 'self' https: data: blob:;
-        child-src 'self' https://www.youtube.com https://www.youtube-nocookie.com https://platform.twitter.com https://www.instagram.com;
+        child-src 'self' 
+          https://www.youtube.com 
+          https://www.youtube-nocookie.com 
+          https://platform.twitter.com 
+          https://www.instagram.com;
       `.replace(/\s+/g, ' ').trim()
     }
   }
